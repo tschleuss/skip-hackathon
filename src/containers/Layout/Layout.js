d4a99f34a1b727bac4f45dcf92afb4df966493f7
Thyago Schleuss <tschleuss@gmail.com>
2018-03-17 19:46:21 -0300

    fix: Links
    - Changing NavLink to Link

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Route } from 'react-router'
+import { Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
 import { ToastContainer } from 'react-toastify'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -10,13 +11,13 @@ const DefaultLayout = ({ component: Component, ...rest }) =>
     <Route {...rest} render={matchProps => (
         <div className="app-container">
             <Navbar className="app-header" color="faded" light expand="md">
-                <NavbarBrand href="/">SkipTheDishes</NavbarBrand>
+                <Link to="/" className="navbar-brand">SkipTheDishes</Link>
                 <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <NavLink href="/auth" className="menu-link">Auth</NavLink>
+                        <Link to="/auth" className="nav-link menu-link">Auth</Link>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="/stores" className="menu-link">Stores</NavLink>
+                        <Link to="/stores" className="nav-link menu-link">Stores</Link>
                     </NavItem>
                 </Nav>
             </Navbar>
